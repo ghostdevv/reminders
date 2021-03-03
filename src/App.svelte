4d87ebaec4dd427af066d57b9850441028708680
GHOST <ghostdevbusiness@gmail.com>
2021-03-03 15:59:20 +0000

    refactor: move service worker registration

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,19 @@
 <script>
     import { Router } from '@roxi/routify';
     import { routes } from '../.routify/routes';
+
+    if ('serviceWorker' in navigator) {
+        import('workbox-window').then(async ({ Workbox }) => {
+            const wb = new Workbox('/serviceworker.js');
+            const registration = await wb.register();
+
+            // Reload the page if the PWA has been updated. Change strategy if needed.
+            wb.addEventListener('redundant', () => {
+                location.reload();
+                console.log('updated app');
+            });
+        });
+    }
 </script>
 
 <Router {routes} />
