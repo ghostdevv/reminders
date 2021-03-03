c3ef98683c64c30a3c6397a4967e921820e7bd61
GHOST <ghostdevbusiness@gmail.com>
2021-03-03 16:08:00 +0000

    fix: service worker link

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
     if ('serviceWorker' in navigator) {
         import('workbox-window').then(async ({ Workbox }) => {
-            const wb = new Workbox('/serviceworker.js');
+            const wb = new Workbox('/service-worker.js');
             const registration = await wb.register();
 
             // Reload the page if the PWA has been updated. Change strategy if needed.
