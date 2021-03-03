851b5c75c3173f38117a9010490c4aa6416f5661
GHOST <ghostdevbusiness@gmail.com>
2021-03-03 15:39:51 +0000

    feat: updated service worker

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -2,7 +2,32 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js',
 );
 
+workbox.precaching.precacheAndRoute([]);
+
+// Caching Images
 workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst(),
+    /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'images',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            }),
+        ],
+    }),
+);
+
+// Cache CSS and JavaScript Files
+workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'static-resources',
+    }),
+);
+
+// Caching Content from Multiple Origins
+workbox.routing.registerRoute(
+    /.*(?:googleapis|gstatic)\.com/,
+    new workbox.strategies.StaleWhileRevalidate(),
 );
