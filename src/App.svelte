39596f1da369815897599a2629e72604003256ed
GHOST <ghostdevbusiness@gmail.com>
2021-03-07 16:06:54 +0000

    fix(revert): service worker

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,6 @@
 <script>
     import { Router } from '@roxi/routify';
     import { routes } from '../.routify/routes';
-
-    if ('serviceWorker' in navigator) {
-        import('workbox-window').then(async ({ Workbox }) => {
-            const wb = new Workbox('/service-worker.js');
-            const registration = await wb.register();
-
-            // Reload the page if the PWA has been updated. Change strategy if needed.
-            wb.addEventListener('redundant', () => {
-                location.reload();
-                console.log('updated app');
-            });
-        });
-    }
 </script>
 
 <Router {routes} />
